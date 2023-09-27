2d71650a563120d81f5b20af9d814caeb88984c8
deepthi.vemula <deepthi.vemula@servicenow.com>
2023-09-27 21:42:16 +0530

    SEP-27: Product Array except the index

---
+++ demo/src/main/java/com/daily_coding_problem/coding_problems/September27.java
@@ -0,0 +1,37 @@
+package com.daily_coding_problem.coding_problems;
+
+/*
+ * Given an array of integers, 
+ * return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+ * For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+ * 
+ * Follow-up: what if you can't use division?
+ */
+public class September27 {
+
+    public int[] productOfAllNumbersExceptIndex(int[] list) {
+        int[] result = new int[list.length];
+        int[] leftPdt = new int[list.length];
+        int[] rightPdt = new int[list.length];
+        //populate Left to Right product array
+        leftPdt[0] = list[0];
+        for(int i = 1; i < list.length; i++) {
+            leftPdt[i] = leftPdt[i-1] * list[i];
+        }
+
+        //populate Right to Left product array
+        rightPdt[list.length-1] = list[list.length-1];
+        for(int i = list.length-2; i >= 0; i--) {
+            rightPdt[i] = rightPdt[i+1] * list[i];
+        }
+        result[0] = rightPdt[1];
+        result[list.length-1] = leftPdt[list.length-2];
+        //populate result array
+        for(int i=1; i < list.length-1; i++){
+            result[i] = leftPdt[i-1] * rightPdt[i+1];
+        }
+        return result;
+        
+    }
+    
+}
--- demo/src/test/java/SeptemberTest.java
+++ demo/src/test/java/SeptemberTest.java
@@ -1,8 +1,8 @@
-import static org.testng.Assert.assertTrue;
-
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 import com.daily_coding_problem.coding_problems.September26;
+import com.daily_coding_problem.coding_problems.September27;
 
 public class SeptemberTest {
     @Test
@@ -10,7 +10,27 @@ public class SeptemberTest {
         September26 september26 = new September26();
         int[] list = {10, 15, 3, 7};
         int k = 17;
-        assertTrue(september26.checkSumInList(k, list), "10 + 7 is 17");
+        Assert.assertTrue(september26.checkSumInList(k, list));
+    }
+
+    @Test
+    public void Sep27TestProductArray() {
+        September27 september27 = new September27();
+        int[] list = {1, 2, 3, 4, 5};
+        int[] expectedResult = {120, 60, 40, 30, 24};
+        int[] result = september27.productOfAllNumbersExceptIndex(list);
+        Assert.assertArrayEquals(expectedResult , result);
+
+        list = new int[]{3, 2, 1};
+        expectedResult = new int[]{2, 3, 6};
+        result = september27.productOfAllNumbersExceptIndex(list);
+        Assert.assertArrayEquals(expectedResult , result);
+
+        list = new int[]{1, 2, 3, 4, 5, 6};
+        expectedResult = new int[]{720, 360, 240, 180, 144, 120};
+        result = september27.productOfAllNumbersExceptIndex(list);
+        Assert.assertArrayEquals(expectedResult , result);
+        
     }
     
 }
